150c1a13f02114d6feaa30816be241cbf6050a67
ussoewwin <ussoewwin@users.noreply.github.com>
2026-06-09 11:43:58 +0900

    \ docs: fix CHANGELOG v1.7.5 ADetailer InsightFace removal\

--- extensions-builtin/adetailer/adetailer/__init__.py
+++ extensions-builtin/adetailer/adetailer/__init__.py
@@ -2,7 +2,7 @@ from .__version__ import __version__
 from .args import ALL_ARGS, ADetailerArgs
 from .common import PredictOutput, get_models, insightface_predict
 from .mediapipe import mediapipe_predict
-from .ultralytics import ultralytics_predict, hybrid_face_predict
+from .ultralytics import ultralytics_predict
 
 ADETAILER = "ADetailer"
 
@@ -16,5 +16,4 @@ __all__ = [
     "mediapipe_predict",
     "ultralytics_predict",
     "insightface_predict",
-    "hybrid_face_predict",
 ]
